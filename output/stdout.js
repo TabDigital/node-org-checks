95c8db089f369d4056e5eb45292a6f321b6cabbe
Yoshua Wuyts <i@yoshuawuyts.com>
2016-01-12 11:58:02 +1100

    output/stdout: add summarize opt

--- output/stdout.js
+++ output/stdout.js
@@ -1,8 +1,20 @@
 module.exports = toStdout
 
-function toStdout () {
+// print to stdout
+// obj -> null
+function toStdout (opts) {
+  opts = opts || {}
   return function (data, cb) {
+    if (opts.summary) data = filter(data)
     console.log(JSON.stringify(data, null, 2))
     cb()
   }
+}
+
+// filter type=summary
+// [obj] -> [obj]
+function filter (data) {
+  return data.filter(function (chunk) {
+    if (chunk.type === 'summary') return chunk
+  })
 }
